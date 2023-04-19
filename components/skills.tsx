ec6b9407e71b85f73d114381ab9f271d77fcaf6f
313devs <66395068+313devs@users.noreply.github.com>
2023-04-19 02:43:23 +0100

    Fixed skills {key} prop error.

--- components/skills.tsx
+++ components/skills.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import data from '../public/data.json';
 
 type skillData = {
+  id: number;
   text: string;
 };
 
@@ -9,7 +10,7 @@ const SkillList: React.FC = () => {
   return (
     <div className="flex flex-row h-fit w-fit flex-wrap">
       {data.map((item: skillData) => (
-        <h1 className="md:text-sm text-lg font-popL ml-0 mb-4 px-1 py-1 text-black 2xl:text-4xl dark:text-white dark:hover:bg-white/20 hover:bg-black/20 rounded-sm cursor-none">{item.text}</h1>
+        <h1 key={item.id} className="md:text-sm text-lg font-popL ml-0 mb-4 px-1 py-1 text-black 2xl:text-4xl dark:text-white dark:hover:bg-white/20 hover:bg-black/20 rounded-sm cursor-none">{item.text}</h1>
       ))}
     </div>
   );
