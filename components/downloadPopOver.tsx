18158611f5ffc3c8a2a607d1df1e646f21f8a1f6
313devs <66395068+313devs@users.noreply.github.com>
2023-04-23 22:25:52 +0100

    Resume file now downloadable

--- components/downloadPopOver.tsx
+++ components/downloadPopOver.tsx
@@ -19,20 +19,30 @@ export default function DownloadButton() {
           className="rounded-lg p-5 w-60 bg-neutral-200 dark:bg-neutral-800 ml-5 flex flex-col justify-center items-center"
           sideOffset={5}
         >
-          <div className="w-38 h-fit rounded-lg mb-2 overflow-hidden flex justify-center items-center 2xl:w-64 2xl:h-64">
+          <div className="w-fit h-fit rounded-lg mb-2 overflow-hidden flex justify-center items-center 2xl:w-64 2xl:h-64">
             <Image
-            src={"/ResumeThumbnail.png"}
-            alt="Thumbnail"
-            width={1000}
-            height={1000}
-            className="w-full h-full object-cover opacity-25"
+              src={"/ResumeThumbnail.png"}
+              alt="Thumbnail"
+              width={1000}
+              height={1000}
+              className="w-full h-full object-cover opacity-25"
             />
-            <h1 className="text-center text-xl font-popL absolute">Resume.pdf</h1>
+            <h1 className="text-center text-xl font-popL absolute">
+              Resume.pdf
+            </h1>
           </div>
-          <button className='h-10 w-full flex justify-center items-center bg-neutral-100 dark:bg-neutral-700 text-lg font-popL rounded-lg active:dark:bg-neutral-800 active:bg-neutral-200'>
-            Download
-            <DownloadIcon className="ml-2" />
-          </button>
+          <a
+            className="flex flex-row items-center justify-center w-full"
+            href="/Resume.pdf"
+            download="Resume.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="h-10 w-full flex justify-center items-center bg-neutral-100 dark:bg-neutral-700 text-lg font-popL rounded-lg active:dark:bg-neutral-800 active:bg-neutral-200">
+              Download
+              <DownloadIcon className="ml-2" />
+            </button>
+          </a>
           <Popover.Arrow className="fill-white" />
         </Popover.Content>
       </Popover.Portal>
